d6d05bcbdda4b94afdbc99f73b2c9b5c20add8c1
Hiromi ISHII <konn.jinro@gmail.com>
2024-06-23 00:09:58 +0900

    Make it impure

--- wasm-linear-base/src/Wasm/Data/Array/Destination/JSVal.hs
+++ wasm-linear-base/src/Wasm/Data/Array/Destination/JSVal.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
@@ -20,6 +21,8 @@ module Wasm.Data.Array.Destination.JSVal (
 
 import Data.Ord (Ord (..))
 import qualified Data.Vector as V
+import GHC.Base (unIO)
+import GHC.Exts (runRW#)
 import GHC.IO (unsafeDupablePerformIO)
 import GHC.Stack
 import GHC.Wasm.Prim
@@ -42,15 +45,16 @@ foreign import javascript unsafe "Array($1)"
   js_new_array :: Int -> System.IO JSArray
 
 foreign import javascript unsafe "$3[$1] = $2"
-  js_set_array :: Int -> JSVal -> JSArray %1 -> ()
+  js_set_array :: Int -> JSVal -> JSArray %1 -> System.IO ()
 
 replicate :: JSVal -> DJSArray %1 -> ()
+{-# NOINLINE replicate #-}
 replicate !v (DJSArray off len arr) = go 0
   where
     go !i
       | i < len =
-          case js_set_array (off + i) v arr of
-            () -> go (i + 1)
+          case runRW# (unIO $ js_set_array (off + i) v arr) of
+            (# _, () #) -> go (i + 1)
       | otherwise = ()
 
 alloc :: Int -> (DJSArray %1 -> ()) %1 -> JSArray
@@ -78,12 +82,13 @@ fill a (DJSArray off len mvec) =
         PL.& Unsafe.toLinear (\x -> js_set_array off x mvec `P.seq` ())
 
 fromFunction :: (Int -> JSVal) -> DJSArray %1 -> ()
+{-# NOINLINE fromFunction #-}
 fromFunction f (DJSArray off len mvec) = go 0
   where
     go !i
       | i < len =
-          case js_set_array (off + i) (f i) mvec of
-            () -> go (i + 1)
+          case runRW# (unIO (js_set_array (off + i) (f i) mvec)) of
+            (# !_, () #) -> go (i + 1)
       | otherwise = ()
 
 mirror :: (HasCallStack) => V.Vector JSVal -> DJSArray %1 -> ()
