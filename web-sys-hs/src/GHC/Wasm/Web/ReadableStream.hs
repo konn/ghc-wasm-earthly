0d445f5a8399f7d8a73de4e537821d388b8d723a
Hiromi ISHII <konn.jinro@gmail.com>
2024-06-25 21:49:35 +0900

    fix: call `read` everytime

--- web-sys-hs/src/GHC/Wasm/Web/ReadableStream.hs
+++ web-sys-hs/src/GHC/Wasm/Web/ReadableStream.hs
@@ -8,27 +8,26 @@ module GHC.Wasm.Web.ReadableStream (
 
 import Control.Monad ((<=<))
 import Control.Monad.Trans.Class (lift)
-import Data.Word (Word8)
+import Data.Word
 import GHC.Wasm.Object.Builtins
+import GHC.Wasm.Prim
 import GHC.Wasm.Web.Generated.ReadableStream
 import GHC.Wasm.Web.Generated.ReadableStreamDefaultReader
-import GHC.Wasm.Web.Generated.ReadableStreamReadResult
 import qualified Streaming.ByteString as Q
 
 fromReadableStream :: ReadableStream -> Q.ByteStream IO ()
 fromReadableStream =
   fromReadResult
     <=< lift
-      . ( await
-            <=< js_fun_read__Promise_ReadableStreamReadResult
-              . unsafeCast @_ @(ReadableStreamDefaultReaderClass)
-            <=< flip
+      . ( fmap (unsafeCast @_ @(ReadableStreamDefaultReaderClass))
+            . flip
               js_fun_getReader_nullable_ReadableStreamGetReaderOptions_ReadableStreamReader
               (toNullable Nothing)
         )
 
-fromReadResult :: ReadableStreamReadResult -> Q.ByteStream IO ()
-fromReadResult = Q.reread \resl -> do
+fromReadResult :: ReadableStreamDefaultReader -> Q.ByteStream IO ()
+fromReadResult = Q.reread \inp -> do
+  resl <- await =<< js_fun_read__Promise_ReadableStreamReadResult inp
   done <- getDictField "done" resl
   value <-
     fmap (toByteString . unsafeCast @_ @(JSByteArrayClass Word8)) . fromNullable
