8f8a78a2e1df0a4bf3383875bc91cd1b6064616b
Hiromi ISHII <konn.jinro@gmail.com>
2024-06-30 11:55:08 +0900

    CF auth

--- steward-client/src/Steward/Client.hs
+++ steward-client/src/Steward/Client.hs
@@ -1,15 +1,19 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Steward.Client (
   runClient,
   runClientWith,
   ClientT (..),
   ClientEnv (..),
+  withRequestModifier,
+  withCloudflareServiceTokenAuth,
 
   -- * Internal functions
   toStewardResponse,
@@ -17,9 +21,10 @@ module Steward.Client (
   makeStewardRequest,
 ) where
 
-import Control.Exception.Safe (Exception, MonadCatch, MonadThrow, throwIO)
+import Control.Exception.Safe (Exception, MonadCatch, MonadMask, MonadThrow, throwIO)
+import Control.Monad ((<=<))
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader.Class (MonadReader, ask)
+import Control.Monad.Reader.Class (MonadReader (local), ask)
 import Control.Monad.Trans.Class (MonadTrans)
 import Control.Monad.Trans.Reader (ReaderT (..))
 import Data.ByteString.Builder qualified as BB
@@ -27,6 +32,7 @@ import Data.ByteString.Char8 qualified as BS8
 import Data.ByteString.Lazy qualified as LBS
 import Data.Foldable qualified as F
 import Data.Functor ((<&>))
+import Data.List qualified as List
 import GHC.Generics (Generic)
 import Network.HTTP.Client (Manager, Request, Response (..), httpLbs, requestFromURI)
 import Network.HTTP.Client qualified as H
@@ -43,7 +49,41 @@ runClient uri act = do
 
 runClientWith :: Manager -> URI -> ClientT m a -> m a
 runClientWith man uri (ClientT act) = do
-  runReaderT act ClientEnv {manager = man, endpoint = uri}
+  runReaderT act ClientEnv {manager = man, endpoint = uri, requestModifier = pure}
+
+data ServiceToken = ServiceToken
+  { clientID :: BS8.ByteString
+  , clientSecret :: BS8.ByteString
+  }
+  deriving (Show, Eq, Ord, Generic)
+
+withCloudflareServiceTokenAuth ::
+  (Monad m) =>
+  ServiceToken ->
+  ClientT m a ->
+  ClientT m a
+withCloudflareServiceTokenAuth tok = do
+  withRequestModifier \req -> do
+    pure
+      req
+        { H.requestHeaders =
+            (cfClientIdHeader, tok.clientID)
+              : (cfClientSecretHeader, tok.clientSecret)
+              : List.filter
+                ((`notElem` [cfClientSecretHeader, cfClientIdHeader]) . fst)
+                (H.requestHeaders req)
+        }
+  where
+    cfClientIdHeader = "CF-Access-Client-Id"
+    cfClientSecretHeader = "CF-Access-Client-Secret"
+
+withRequestModifier ::
+  (Monad m) =>
+  (Request -> IO Request) ->
+  ClientT m a ->
+  ClientT m a
+withRequestModifier f act = do
+  local (\env -> env {requestModifier = f <=< env.requestModifier}) act
 
 newtype ClientT m a = ClientT {unClientT :: ReaderT ClientEnv m a}
   deriving (Generic)
@@ -55,12 +95,14 @@ newtype ClientT m a = ClientT {unClientT :: ReaderT ClientEnv m a}
     , MonadTrans
     , MonadThrow
     , MonadCatch
+    , MonadMask
     , MonadReader ClientEnv
     )
 
 data ClientEnv = ClientEnv
   { manager :: !Manager
   , endpoint :: !URI
+  , requestModifier :: Request -> IO Request
   }
   deriving (Generic)
 
@@ -71,7 +113,7 @@ instance (MonadThrow m, MonadIO m) => MonadClient (ClientT m) where
 
 makeStewardRequest :: ClientEnv -> PartialRequest -> IO StewardResponse
 makeStewardRequest env req = do
-  req' <- fromStewardRequest env.endpoint req
+  req' <- env.requestModifier =<< fromStewardRequest env.endpoint req
   toStewardResponse =<< httpLbs req' env.manager
 
 fromStewardRequest :: (MonadThrow m) => URI -> PartialRequest -> m Request
