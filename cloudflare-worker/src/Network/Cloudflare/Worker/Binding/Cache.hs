e3514f87afd3c57ee5a03d51ee29176fa5f47645
Hiromi ISHII <konn.jinro@gmail.com>
2024-07-24 20:18:38 +0900

    refactor: WorkerResponse and Requests

--- cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Cache.hs
+++ cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Cache.hs
@@ -36,8 +36,8 @@ module Network.Cloudflare.Worker.Binding.Cache (
 
 import Control.Monad ((<=<))
 import GHC.Wasm.Object.Builtins
-import GHC.Wasm.Web.Generated.Request (Request, RequestClass)
-import GHC.Wasm.Web.Generated.Response (Response, ResponseClass)
+import Network.Cloudflare.Worker.Request (WorkerRequest, WorkerRequestClass)
+import Network.Cloudflare.Worker.Response (WorkerResponse, WorkerResponseClass)
 
 type data WorkersCacheClass :: Prototype
 
@@ -45,11 +45,11 @@ type instance SuperclassOf WorkersCacheClass = 'Nothing
 
 type WorkersCache = JSObject WorkersCacheClass
 
-put :: Request -> Response -> IO (Promise UndefinedClass)
+put :: WorkerRequest -> WorkerResponse -> IO (Promise UndefinedClass)
 put = putTo defaultCache
 
 foreign import javascript safe "$1.put($2, $3)"
-  putTo :: WorkersCache -> Request -> Response -> IO (Promise UndefinedClass)
+  putTo :: WorkersCache -> WorkerRequest -> WorkerResponse -> IO (Promise UndefinedClass)
 
 type WorkersCacheOptionsFields =
   '[ '("ignoreMethod", JSPrimClass Bool)]
@@ -59,13 +59,13 @@ type WorkersCacheOptionsClass = JSDictionaryClass WorkersCacheOptionsFields
 type WorkersCacheOptions = JSObject WorkersCacheOptionsClass
 
 match ::
-  Union '[RequestClass, JSByteStringClass] ->
+  Union '[WorkerRequestClass, JSByteStringClass] ->
   Maybe WorkersCacheOptions ->
-  IO (Promise (NullableClass ResponseClass))
+  IO (Promise (NullableClass WorkerResponseClass))
 match req = undefinedToNullPromise <=< rawMatch defaultCache req . toNullable
 
 delete ::
-  Union '[RequestClass, JSByteStringClass] ->
+  Union '[WorkerRequestClass, JSByteStringClass] ->
   Maybe WorkersCacheOptions ->
   IO (Promise (JSPrimClass Bool))
 delete req = js_delete defaultCache req . toNullable
@@ -73,14 +73,14 @@ delete req = js_delete defaultCache req . toNullable
 foreign import javascript safe "$1.match($2, $3)"
   rawMatch ::
     WorkersCache ->
-    Union '[RequestClass, JSByteStringClass] ->
+    Union '[WorkerRequestClass, JSByteStringClass] ->
     Nullable WorkersCacheOptionsClass ->
-    IO (Promise (UnionClass '[ResponseClass, UndefinedClass]))
+    IO (Promise (UnionClass '[WorkerResponseClass, UndefinedClass]))
 
 foreign import javascript safe "$1.delete($2, $3)"
   js_delete ::
     WorkersCache ->
-    Union '[RequestClass, JSByteStringClass] ->
+    Union '[WorkerRequestClass, JSByteStringClass] ->
     Nullable WorkersCacheOptionsClass ->
     IO (Promise (JSPrimClass Bool))
 
