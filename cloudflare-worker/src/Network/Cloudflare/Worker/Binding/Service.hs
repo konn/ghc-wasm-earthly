da8cd6909abc8fc124aa42495fc37475f6ff5a9c
Hiromi ISHII <konn.jinro@gmail.com>
2024-10-28 21:52:04 +0900

    Service Binding with contexts

--- cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Service.hs
+++ cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Service.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveAnyClass #-}
@@ -24,6 +25,13 @@
 -- | Service Binding for custom RPC between Cloudflare Workers.
 module Network.Cloudflare.Worker.Binding.Service (
   FunSig (..),
+  ServiceM,
+  getBinding,
+  getEnv,
+  getRawEnv,
+  getSecret,
+  getFetchContext,
+  waitUntil,
   type (~>),
   ToWorkerFun,
   ToCallerFun,
@@ -43,18 +51,23 @@ module Network.Cloudflare.Worker.Binding.Service (
   -- * Internal
   FromFunSig,
   FunMode (..),
-  ToHsRet,
 ) where
 
-import Control.Exception.Safe (Exception, throwIO)
+import Control.Arrow ((>>>))
+import Control.Exception.Safe (Exception, MonadCatch, MonadMask, MonadThrow, throwIO)
 import Control.Monad (join, (<=<))
+import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Except (ExceptT (..), runExceptT)
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson qualified as J
 import Data.Coerce (coerce)
 import Data.Int
 import Data.Kind (Constraint, Type)
+import Data.Text qualified as T
+import Data.Type.Bool (If, type (&&))
 import Data.Vector qualified as V
 import Data.Word
+import Effectful (MonadUnliftIO)
 import GHC.Exts (Proxy#, proxy#)
 import GHC.Generics
 import GHC.Records
@@ -64,10 +77,60 @@ import GHC.Wasm.Object.Builtins
 import GHC.Wasm.Prim
 import GHC.Wasm.Web.JSON
 import Language.WASM.JSVal.Convert
+import Network.Cloudflare.Worker.Binding (BindingsClass, ListMember)
+import Network.Cloudflare.Worker.Binding qualified as B
+import Network.Cloudflare.Worker.Handler.Fetch (FetchContext)
 
 type data FunSig = Return Type | Type :~> FunSig
 
 type data JSFunSig = ReturnJS Prototype | Prototype :~>> JSFunSig
+
+type ServiceM :: Prototype -> [(Symbol, FunSig)] -> Type -> Type
+newtype ServiceM e bs a = ServiceM {runServiceM :: IO a}
+  deriving newtype
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadIO
+    , MonadUnliftIO
+    , MonadThrow
+    , MonadCatch
+    , MonadMask
+    )
+
+getBinding ::
+  forall es ss bs fs.
+  forall l ->
+  (KnownSymbol l, Member l bs) =>
+  ServiceM (BindingsClass es ss bs) fs (JSObject (Lookup' l bs))
+getBinding l = B.getBinding l <$> js_get_env
+
+getSecret ::
+  forall es ss bs fs.
+  forall l ->
+  (KnownSymbol l, ListMember l ss) =>
+  ServiceM (BindingsClass es ss bs) fs T.Text
+getSecret l = B.getSecret l <$> js_get_env
+
+getEnv ::
+  forall es ss bs fs a.
+  forall l ->
+  (KnownSymbol l, ListMember l es, FromJSON a) =>
+  ServiceM (BindingsClass es ss bs) fs a
+getEnv l =
+  getRawEnv l
+    >>= ( J.fromJSON >>> \case
+            J.Error e -> throwIO $ FunResultDecodeFailure e
+            J.Success x -> pure x
+        )
+
+getRawEnv ::
+  forall es ss bs fs.
+  forall l ->
+  (KnownSymbol l, ListMember l es) =>
+  ServiceM (BindingsClass es ss bs) fs J.Value
+getRawEnv l =
+  B.getEnv l <$> js_get_env
 
 call ::
   forall bs x.
@@ -76,7 +139,7 @@ call ::
   Service bs ->
   ToCallerFun x
 call l srv =
-  joinHsFun Caller x $
+  joinHsFun x $
     decodeFun# (proxy# @x)
       <$> js_get_fun @bs @(ToJSFunSig x) srv (toJSString $ symbolVal' @l proxy#)
 
@@ -96,7 +159,7 @@ type (~>) = (:~>)
 
 infixr 5 :~>, :~>>, ~>
 
-data FunMode = Caller | Defn
+data FunMode = Caller | Defn Prototype [(Symbol, FunSig)]
 
 data ServiceBindingException
   = FunResultDecodeFailure !String
@@ -104,16 +167,13 @@ data ServiceBindingException
   deriving (Show, Eq, Ord, Generic)
   deriving anyclass (Exception)
 
-type family ToHsRet mode a where
-  ToHsRet Caller a = Promised (ServiceArg a) a
-  ToHsRet Defn a = JSObject (ServiceArg a)
-
-type ToWorkerFun a = FromFunSig Defn a
+type ToWorkerFun e fs a = FromFunSig (Defn e fs) a
 
 type ToCallerFun a = FromFunSig Caller a
 
 type family FromFunSig mode fn where
-  FromFunSig mode (Return a) = IO (ToHsRet mode a)
+  FromFunSig (Defn e fs) (Return a) = ServiceM e fs a
+  FromFunSig Caller (Return a) = IO (Promised (ServiceArg a) a)
   FromFunSig mode (a :~> b) = a -> FromFunSig mode b
 
 type family ToJSFunSig fn = js where
@@ -122,16 +182,21 @@ type family ToJSFunSig fn = js where
 
 type IsServiceFunSig :: FunSig -> Constraint
 class IsServiceFunSig fn where
-  encodeJSFun# :: Proxy# fn -> FromFunSig Defn fn -> IO (JSFun (ToJSFunSig fn))
-  decodeFun# :: Proxy# fn -> JSFun (ToJSFunSig fn) -> FromFunSig Caller fn
-  joinHsFun# :: Proxy# fn -> Proxy# mode -> IO (FromFunSig mode fn) -> FromFunSig mode fn
+  encodeJSFun# ::
+    Proxy# fn ->
+    Proxy# e ->
+    Proxy# fs ->
+    FromFunSig (Defn e fs) fn ->
+    IO (JSFun (ToJSFunSig fn))
+  decodeFun# :: Proxy# fn -> JSFun (ToJSFunSig fn) -> ToCallerFun fn
+  joinHsFun# :: Proxy# fn -> IO (FromFunSig Caller fn) -> FromFunSig Caller fn
 
-joinHsFun :: forall mode fn -> (IsServiceFunSig fn) => IO (FromFunSig mode fn) -> FromFunSig mode fn
+joinHsFun :: forall fn -> (IsServiceFunSig fn) => IO (ToCallerFun fn) -> ToCallerFun fn
 {-# INLINE joinHsFun #-}
-joinHsFun mode fn = joinHsFun# (proxy# @fn) (proxy# @mode)
+joinHsFun fn = joinHsFun# (proxy# @fn)
 
 instance (IsServiceArg a) => IsServiceFunSig (Return a) where
-  encodeJSFun# _ = coerce
+  encodeJSFun# _ _ _ = (coerce . encodeServiceArg @a =<<) . (.runServiceM)
   {-# INLINE encodeJSFun# #-}
   decodeFun# _ =
     pure
@@ -141,19 +206,19 @@ instance (IsServiceArg a) => IsServiceFunSig (Return a) where
         )
       . unsafeCast @_ @(PromiseClass (ServiceArg a))
   {-# INLINE decodeFun# #-}
-  joinHsFun# _ _ = join
+  joinHsFun# _ = join
   {-# INLINE joinHsFun# #-}
 
 instance (IsServiceArg a, IsServiceFunSig bs) => IsServiceFunSig (a :~> bs) where
-  encodeJSFun# _ f = js_ffi_fun_arrow @(ServiceArg a) @(ToJSFunSig bs) $ \x -> do
+  encodeJSFun# _ e fs f = js_ffi_fun_arrow @(ServiceArg a) @(ToJSFunSig bs) $ \x -> do
     xjs <- either (throwIO . FunArgDecodeFailure) pure =<< parseServiceArg @a x
-    encodeJSFun# (proxy# @bs) (f xjs)
+    encodeJSFun# (proxy# @bs) e fs (f xjs)
 
-  decodeFun# _ f x = joinHsFun Caller bs do
+  decodeFun# _ f x = joinHsFun bs do
     xjs <- encodeServiceArg x
     pure $ decodeFun# (proxy# @bs) $ js_ffi_app_fun f xjs
 
-  joinHsFun# _ (_ :: Proxy# mode) f x = joinHsFun mode bs $ ($ x) <$> f
+  joinHsFun# _ f x = joinHsFun bs $ ($ x) <$> f
   {-# INLINE joinHsFun# #-}
 
 type data ServiceClass :: [(Symbol, FunSig)] -> Prototype
@@ -168,81 +233,116 @@ type JSFun fn = JSObject (JSFunClass fn)
 If you have a function with many arguments, this can lead to performance degression.
 In such cases, we recommend to write the instance manually.
 -}
-class ToService a where
-  type Signature a :: [(Symbol, FunSig)]
-  type Signature a = GSignature (Rep a)
+class ToService e a where
+  type Signature e a :: [(Symbol, FunSig)]
+  type Signature e a = GSignature e (Rep a)
 
-  toService :: a -> IO (JSObject (ServiceClass (Signature a)))
+  toService :: a -> IO (Service (Signature e a))
   default toService ::
-    (GenericToService a) =>
+    (GenericToService e a) =>
     a ->
-    IO (JSObject (ServiceClass (Signature a)))
-  toService = genericToService
+    IO (JSObject (ServiceClass (Signature e a)))
+  toService = genericToService @e
 
-type GenericToService a =
+type GenericToService e a =
   ( Generic a
-  , GToService (Rep a)
-  , Signature a ~ GSignature (Rep a)
+  , GToService e (GSignature e (Rep a)) (Rep a)
+  , Signature e a ~ GSignature e (Rep a)
   )
 
 genericToService ::
-  (GenericToService a) =>
+  forall e a.
+  (GenericToService e a) =>
   a ->
-  IO (JSObject (ServiceClass (GSignature (Rep a))))
+  IO (Service (GSignature e (Rep a)))
 genericToService a = do
   sink <- js_new_service_sink
-  gwriteField sink $ from a
+  gwriteField (proxy# @e) (proxy# @(GSignature e (Rep a))) sink $ from a
   pure sink.runServiceSink
 
-class GToService f where
-  type GSignature f :: [(Symbol, FunSig)]
-  gwriteField :: ServiceSink (GSignature f) -> f a -> IO ()
+type GToService :: Prototype -> [(Symbol, FunSig)] -> (Type -> Type) -> Constraint
+class GToService e fs f where
+  gwriteField :: Proxy# e -> Proxy# fs -> ServiceSink (GSignature e f) -> f a -> IO ()
 
-instance GToService U1 where
-  type GSignature U1 = '[]
-  gwriteField _ _ = pure ()
+instance GToService e fs U1 where
+  gwriteField _ _ _ _ = pure ()
 
 castSink :: forall fs fs0. ServiceSink fs0 -> ServiceSink fs
 castSink = ServiceSink . unsafeCast . (.runServiceSink)
 
-type family ToFunSig a where
-  ToFunSig (IO a) = Return a
-  ToFunSig (a -> b) = a ~> ToFunSig b
+type family ToFunSig e a where
+  ToFunSig _ (IO a) = Return a
+  ToFunSig e' (ServiceM e fs a) = Return a
+  ToFunSig e (a -> b) = a ~> ToFunSig e b
 
-class (IsServiceFunSig (ToFunSig a)) => IsWorkerFun a where
-  toWorkerFun :: a -> ToWorkerFun (ToFunSig a)
+type family fs ⊆? fs' where
+  xs ⊆? xs = 'True
+  '[] ⊆? _ = 'True
+  (f ': fs) ⊆? fs' = Elem f fs' && fs ⊆? fs'
 
-instance (IsServiceArg a) => IsWorkerFun (IO a) where
-  toWorkerFun = (encodeServiceArg =<<)
-  {-# INLINE toWorkerFun #-}
+type family Elem f fs where
+  Elem f (f ': _) = 'True
+  Elem f (_ ': fs) = Elem f fs
+  Elem _ '[] = 'False
 
-instance (IsServiceArg a, IsWorkerFun b) => IsWorkerFun (a -> b) where
-  toWorkerFun f = \x -> toWorkerFun (f x)
+type (⊆) :: [a] -> [a] -> Constraint
+type family fs ⊆ fs' where
+  fs ⊆ fs' = If (fs ⊆? fs') (() :: Constraint) (Unsatisfiable ('Text "fs is not a subset of fs'"))
+
+type family GSignature e f where
+  GSignature e (S1 ('MetaSel 'Nothing _a _b _c) _d) = TypeError ('Text "Record field must have a label")
+  GSignature e (S1 ('MetaSel ('Just l) _a _b _c) (K1 i c)) = '[ '(l, ToFunSig e c)]
+  GSignature e (D1 i f) = GSignature e f
+  GSignature e (C1 i f) = GSignature e f
+  GSignature e (f :*: g) = GSignature e f ++ GSignature e g
+  GSignature e (f :+: g) = TypeError ('Text "Sum type cannot be marshaled into a service binding")
+
+class (IsServiceFunSig (ToFunSig e a)) => IsWorkerFun e fs a where
+  toWorkerFun :: Proxy# e -> Proxy# fs -> a -> ToWorkerFun e fs (ToFunSig e a)
+
+instance
+  (IsServiceArg a) =>
+  IsWorkerFun e fs (IO a)
+  where
+  toWorkerFun _ _ = ServiceM
   {-# INLINE toWorkerFun #-}
 
 instance
-  ( KnownSymbol l
-  , IsWorkerFun c
-  ) =>
-  GToService (S1 ('MetaSel ('Just l) _pk _st _d) (K1 i c))
+  (IsServiceArg a, e ~ e', fs' ⊆ fs) =>
+  IsWorkerFun e fs (ServiceM e' fs' a)
   where
-  type GSignature (S1 ('MetaSel ('Just l) _pk _st _d) (K1 i c)) = '[ '(l, ToFunSig c)]
-  gwriteField sink (M1 (K1 x)) = js_set_handler sink (toJSString $ symbolVal' @l proxy#) =<< encodeJSFun# (proxy# @(ToFunSig c)) (toWorkerFun x)
+  toWorkerFun _ _ = coerce
+  {-# INLINE toWorkerFun #-}
 
-instance (GToService f) => GToService (D1 i f) where
-  type GSignature (D1 i f) = GSignature f
-  gwriteField sink (M1 x) = gwriteField sink x
+instance (IsServiceArg a, IsWorkerFun e fs b) => IsWorkerFun e fs (a -> b) where
+  toWorkerFun e fs f = \x -> toWorkerFun e fs (f x)
+  {-# INLINE toWorkerFun #-}
 
-instance (GToService f) => GToService (C1 i f) where
-  type GSignature (C1 i f) = GSignature f
-  gwriteField sink (M1 x) = gwriteField sink x
+instance
+  ( Unsatisfiable ('Text "Record field must have a label")
+  ) =>
+  GToService e fs (S1 ('MetaSel 'Nothing _pk _st _d) (K1 i c))
 
-instance (GToService f, GToService g) => GToService (f :*: g) where
-  type GSignature (f :*: g) = GSignature f ++ GSignature g
-  gwriteField sink (f :*: g) = gwriteField (castSink @(GSignature f) sink) f >> gwriteField (castSink @(GSignature g) sink) g
+instance
+  ( KnownSymbol l
+  , IsWorkerFun e fs c
+  ) =>
+  GToService e fs (S1 ('MetaSel ('Just l) _pk _st _d) (K1 i c))
+  where
+  gwriteField e fs sink (M1 (K1 x)) = js_set_handler sink (toJSString $ symbolVal' @l proxy#) =<< encodeJSFun# (proxy# @(ToFunSig e c)) e fs (toWorkerFun (proxy# @e) (proxy# @fs) x)
 
-instance (Unsatisfiable ('Text "Sum type cannot be marshaled into a service binding")) => GToService (f :+: g) where
-  type GSignature (f :+: g) = TypeError ('Text "Sum type cannot be marshaled into a service binding")
+instance (GToService e fs f) => GToService e fs (D1 i f) where
+  gwriteField e fs sink (M1 x) = gwriteField e fs sink x
+
+instance (GToService e fs f) => GToService e fs (C1 i f) where
+  gwriteField e fs sink (M1 x) = gwriteField e fs sink x
+
+instance (GToService e fs f, GToService e fs g) => GToService e fs (f :*: g) where
+  gwriteField e fs sink (f :*: g) =
+    gwriteField e fs (castSink @(GSignature e f) sink) f
+      >> gwriteField e fs (castSink @(GSignature e g) sink) g
+
+instance (Unsatisfiable ('Text "Sum type cannot be marshaled into a service binding")) => GToService e fs (f :+: g)
 
 type family ls ++ rs where
   '[] ++ rs = rs
@@ -353,3 +453,18 @@ foreign import javascript unsafe "$1($2)"
 
 foreign import javascript unsafe "$1[$2]"
   js_get_fun :: Service fs -> JSString -> IO (JSFun f)
+
+foreign import javascript unsafe "this.env"
+  js_get_env :: ServiceM e fs (JSObject e)
+
+foreign import javascript unsafe "this.ctx"
+  getFetchContext :: ServiceM e fs FetchContext
+
+foreign import javascript unsafe "this.ctx.waitUntil($1)"
+  waitUntil :: Promise a -> ServiceM e fs ()
+
+data Add = Add
+  { add :: Int -> Int -> IO Int
+  , sub :: Int -> Int -> ServiceM AnyClass ('[ '("add", Int ~> Int ~> Return Int)]) Int
+  }
+  deriving (Generic, ToService AnyClass)
