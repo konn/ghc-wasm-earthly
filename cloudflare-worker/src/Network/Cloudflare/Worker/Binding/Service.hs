abd2879c0860dd4f18eb70320bf094deea979db9
Hiromi ISHII <konn.jinro@gmail.com>
2024-10-29 21:10:34 +0900

    Inspect decoding

--- cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Service.hs
+++ cloudflare-worker/src/Network/Cloudflare/Worker/Binding/Service.hs
@@ -64,7 +64,9 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.Aeson qualified as J
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as LBS
+import Data.ByteString.Lazy.Char8 qualified as LBS8
 import Data.Coerce (coerce)
+import Data.Function ((&))
 import Data.Int
 import Data.Kind (Constraint, Type)
 import Data.Text qualified as T
@@ -131,9 +133,10 @@ getEnv ::
   ServiceM (BindingsClass es ss bs) fs a
 getEnv l =
   getRawEnv l
-    >>= ( J.fromJSON >>> \case
-            J.Error e -> throwIO $ FunResultDecodeFailure e
-            J.Success x -> pure x
+    >>= ( \a ->
+            J.fromJSON a & \case
+              J.Error e -> throwIO $ FunResultDecodeFailure e $ LBS8.unpack $ J.encode a
+              J.Success x -> pure x
         )
 
 getRawEnv ::
@@ -182,8 +185,8 @@ infixr 5 :~>, :~>>, ~>
 data FunMode = Caller | Defn Prototype [(Symbol, FunSig)]
 
 data ServiceBindingException
-  = FunResultDecodeFailure !String
-  | FunArgDecodeFailure !String
+  = FunResultDecodeFailure !String !String
+  | FunArgDecodeFailure !String !String
   deriving (Show, Eq, Ord, Generic)
   deriving anyclass (Exception)
 
@@ -224,8 +227,14 @@ instance (IsServiceArg a) => IsServiceFunSig (Return a) where
   decodeFun# _ =
     fmap
       ( Promised
-          ( either (throwIO . FunResultDecodeFailure) pure
-              <=< parseServiceArg
+          ( \e -> do
+              either
+                ( \exc ->
+                    throwIO . FunResultDecodeFailure exc . fromJSString
+                      =<< stringify e
+                )
+                pure
+                =<< parseServiceArg e
           )
       )
       . js_call_ret
@@ -235,7 +244,7 @@ instance (IsServiceArg a) => IsServiceFunSig (Return a) where
 
 instance (IsServiceArg a, IsServiceFunSig bs) => IsServiceFunSig (a :~> bs) where
   encodeJSFun# _ (e :: Proxy# e) (fs :: Proxy# fs) f this = js_ffi_fun_arrow @(ServiceArg a) @(ToJSFunSig (Defn e fs) bs) $ \x -> do
-    xjs <- either (throwIO . FunArgDecodeFailure) pure =<< parseServiceArg @a x
+    xjs <- either (\exc -> throwIO . FunArgDecodeFailure exc . fromJSString =<< stringify x) pure =<< parseServiceArg @a x
     encodeJSFun# (proxy# @bs) e fs (f xjs) this
 
   decodeFun# _ f x = joinHsFun bs do
@@ -536,3 +545,6 @@ foreign import javascript unsafe "$1.ctx"
 
 foreign import javascript unsafe "$1.ctx.waitUntil($2)"
   js_raw_waitUntil :: ServiceHandler e -> Promise a -> IO ()
+
+foreign import javascript unsafe "JSON.stringify($1)"
+  stringify :: JSObject e -> IO JSString
